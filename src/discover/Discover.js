7324e03de38001ca45b1e2991f7c962f947856d0
Bella Zhan <bellazhan2021@u.northwestern.edu>
2021-05-15 11:19:31 -0500

    styling on discover

--- src/discover/Discover.js
+++ src/discover/Discover.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-
+import { makeStyles } from "@material-ui/core/styles";
 import data from "./data.json";
 const axios = require("axios");
 
 const Discover = () => {
+  const classes = useStyles();
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     data["best-pictures"].forEach((id) => {
@@ -15,7 +16,7 @@ const Discover = () => {
     });
   }, []);
   return (
-    <div>
+    <div className={classes.movieGrid}>
       {movies.map((movie) => (
         <MoviePoster key={movie.imdbID} data={movie} />
       ))}
@@ -24,12 +25,28 @@ const Discover = () => {
 };
 
 const MoviePoster = ({ data }) => {
+  const classes = useStyles();
   return (
-    <div className="movie-card">
-      <img height={400} src={data["Poster"]} alt={data["Title"] + "poster"} />
-      <h2>{data["Title"]}</h2>
+    <div className={classes.movieCard}>
+      <img height={300} src={data["Poster"]} alt={data["Title"] + "poster"} />
+      <p>{data["Title"]}</p>
     </div>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  movieCard: {
+    border: "1px solid lightgrey",
+    margin: 10,
+    width: 250,
+  },
+  movieGrid: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    padding: 25,
+    justifyContent: "center",
+  },
+}));
 
 export default Discover;
